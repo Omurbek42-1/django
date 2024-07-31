be0a23e55330be8fb57a255d96043185477ef78d
omurbek <omurbekzaanbaeva@gmail.com>
2024-07-31 08:07:37 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,59 +1,65 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from models import User
+from forms import RegistrationForm, LoginForm, ProfileForm
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+@login_required
+def home():
+    return render_template('home.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        hashed_password = generate_password_hash(password, method='sha256')
-
-        new_user = User(username=username, password=hashed_password)
-        db.session.add(new_user)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
         db.session.commit()
-        flash('Registration successful! Please login.')
-        return redirect(url_for('login'))
-
-    return render_template('register.html')
+        login_user(user)
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-
-        if user and check_password_hash(user.password, password):
-            session['user_id'] = user.id
-            flash('Login successful!')
-            return redirect(url_for('profile'))
-        else:
-            flash('Login failed. Check your username and/or password.')
-
-    return render_template('login.html')
-
-@app.route('/profile')
-def profile():
-    if 'user_id' in session:
-        user = User.query.get(session['user_id'])
-        return render_template('profile.html', user=user)
-    return redirect(url_for('login'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            return redirect(url_for('home'))
+    return render_template('login.html', form=form)
 
 @app.route('/logout')
+@login_required
 def logout():
-    session.pop('user_id', None)
-    flash('You have been logged out.')
+    logout_user()
     return redirect(url_for('login'))
 
+@app.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    form = ProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    form.username.data = current_user.username
+    form.email.data = current_user.email
+    return render_template('profile.html', form=form)
+
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,7 +1,20 @@
-from django import forms
-from .models import Post
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
 
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['title', 'content']
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class ProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Update Profile')
